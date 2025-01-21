6091e1f909582fc2974d490055b8ef60b0c03b68
Sothyra Chan <schan256@my.centennialcollege.ca>
2025-01-20 22:22:18 -0500

    used canvas api and declared a function for responsive styling

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 // script.js
 
+const startBtn = document.getElementById("start-btn");
+const canvas = document.getElementById("canvas");
+const startScreen = document.querySelector(".start-screen");
+const checkpointScreen = document.querySelector(".checkpoint-screen");
+const checkpointMessage = document.querySelector(".checkpoint-screen > p"); // Get the paragraph element inside the checkpoint screen
+// Canvas API can be used to create graphics in games using JavaScript and the HTML canvas element
+// getContext method which will provide the context for where the graphics will be rendered
+const ctx = canvas.getContext("2d"); 
+// innerWidth property is a number that represents the interior width of the browser window.
+canvas.width = innerWidth;
+// innerHeight property is a number that represents the interior height of the browser window.
+canvas.height = innerHeight;
+const gravity = 0.5;
+let isCheckpointCollisionDetectionActive = true;
+
+const proportionalSize = (size) => {
+  
+};
